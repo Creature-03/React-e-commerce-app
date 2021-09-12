e3d1cbf4c06fe34c800ebe12a1dddaede63e9c85
Creature <Creature.Creations@outlook.com>
2021-09-12 02:19:11 -0700

    added commerce.js functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import Products from './components/Products/Products/Products';
 import Navbar from './components/Products/Navbar/Navbar';
+import { commerce } from './lib/commerce';
 
 const App = () => {
+    const [products, setProducts] = useState([]);
+
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+
+        setProducts(data);
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
     return (
         <div>
             <Navbar />
