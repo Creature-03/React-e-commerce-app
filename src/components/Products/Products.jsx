37023d5f62d022d73e83d2e1d6c40f5ca98034db
Creature <Creature.Creations@outlook.com>
2021-09-12 20:13:10 -0700

    created cart, implimented router, connected routes

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -9,16 +9,16 @@ const products = [
     { id: 2, name: 'Macbook', description: 'Apple Macbook', price: '$500', image: 'https://images.unsplash.com/photo-1517336714731-489689fd1ca8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=926&q=80', }
 ];
 
-const Products = () => {
+const Products = ({ products, onAddToCart }) => {
     const classes = useStyles();
 
     return(
         <main className={classes.content}>
             <div className='classes.toolbar' />
-            <Grid container justify='center' spacing={4}>
+            <Grid container justifyContent='center' spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                        <Product product={product} />
+                        <Product product={product} onAddToCart={onAddToCart} />
                     </Grid>
                 ))}
             </Grid>
